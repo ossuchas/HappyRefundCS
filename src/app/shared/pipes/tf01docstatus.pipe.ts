95418606f232b131bc7d861c252b968549e1dba4
เรารักเมียหลวง <>
2020-10-20 10:12:10 +0700

    modfiied#2

--- src/app/shared/pipes/tf01docstatus.pipe.ts
+++ src/app/shared/pipes/tf01docstatus.pipe.ts
@@ -6,13 +6,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class Tf01docstatusPipe implements PipeTransform {
     transform(value: any, args?: any): any {
         if (value === 'N') {
-            return 'รอการส่งเอกสาร';
+            return 'รอการส่งเอกสาร/Waiting for documents to be sent';
         } else if (value === 'Y') {
-            return 'รอการตรวจสอบเอกสาร';
+            return 'รอการตรวจสอบเอกสาร/Considering documents';
         } else if (value === 'R') {
-            return 'เอกสารไม่ถูกต้องกรุณาส่งเอกสารใหม่';
+            return 'เอกสารไม่ถูกต้องกรุณาส่งเอกสารใหม่/Invalid document, please submit new document.';
         } else if (value === 'P') {
-            return 'รอการยืนยันวันที่เงินเข้า';
+            return 'รอการยืนยันวันที่เงินเข้า/Wait for confirmation of the deposit date';
         } else if (value === 'A') {
             return 'APPROVED';
         } else {
