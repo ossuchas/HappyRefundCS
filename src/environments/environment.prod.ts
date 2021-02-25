f8295f6fb86d77b192d152d4d96680481d7f89e6
เรารักเมียหลวง <>
2021-02-25 21:25:23 +0700

    modified#3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-//   apiUrl: 'http://192.168.0.42:5000/api/v1'
-  apiUrl: 'http://happyrefundapi-happyrefunds.devops-app.apthai.com/api/v1'
+  apiUrl: 'http://happyrefund-api-testrepo.devops-app.apthai.com/api/v1'
+  // apiUrl: 'http://happyrefundapi-happyrefunds.devops-app.apthai.com/api/v1'
   // apiUrl: 'https://happyrefundapi.apthai.com/api/v1'
 };
