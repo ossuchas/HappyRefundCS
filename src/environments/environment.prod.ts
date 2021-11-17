0a8cafc02081f0774c244274a3cfde52c7f45ae7
Teerapat Thongkomsuk <teerapat_t@apthai.com>
2021-11-17 08:54:05 +0700

    deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,26 +1,26 @@
 export const environment = {
 
 // //============================production============================//
-// production: true,
-// apiUrl: 'https://happyrefundapi.apthai.com/api/v1',
-// apiCRM: 'https://identity-api-crmrevo.apthai.com/api/Token/ClientLogin',
-// apiCRMMaster: 'https://masterdata-api-crmrevo.apthai.com/api/MasterCenters/BankBranchBOTDropdownList',
-// apiCRMRefund: 'https://refund-api-crmrevo.apthai.com/api/Refund/',
-// apiCRMTransferPromotion: 'https://promotion-api-crmrevo.apthai.com/api/TransferPromotions/',
-// apiCRMSalePromotion: 'https://saleapp-crmrevo.apthai.com/api/Agreement/',
-// clientId: 'crmdigital',
-// clientSecret: 'pVcySkP6M2QFYvJm5h7fCjSQPoJkUIOmA1OBqZebj4orj8OX6E1U4xViZTu7VBxGKXcvmEiyipm7PO8HQfNPqZGv6v6WTQ',
-
-// ===============================DEV===============================//
-production: false,
-apiUrl: 'http://happyrefund-api-testrepo.devops-app.apthai.com/api/v1',
-apiCRM: 'http://crmrevo-identity-api-crmrevo-dev.devops-app.apthai.com/api/Token/ClientLogin',
-apiCRMMaster: 'http://crmrevo-masterdata-api-crmrevo-dev.devops-app.apthai.com/api/MasterCenters/BankBranchBOTDropdownList',
-apiCRMRefund: 'http://crmrevo-refund-api-crmrevo-dev.devops-app.apthai.com/api/Refund/',
-apiCRMTransferPromotion: 'http://crmrevo-promotion-api-crmrevo-dev.devops-app.apthai.com/api/TransferPromotions/',
-apiCRMSalePromotion: 'http://crmrevo-sale-api-crmrevo-dev.devops-app.apthai.com/api/Agreement/',
+production: true,
+apiUrl: 'https://happyrefundapi.apthai.com/api/v1',
+apiCRM: 'https://identity-api-crmrevo.apthai.com/api/Token/ClientLogin',
+apiCRMMaster: 'https://masterdata-api-crmrevo.apthai.com/api/MasterCenters/BankBranchBOTDropdownList',
+apiCRMRefund: 'https://refund-api-crmrevo.apthai.com/api/Refund/',
+apiCRMTransferPromotion: 'https://promotion-api-crmrevo.apthai.com/api/TransferPromotions/',
+apiCRMSalePromotion: 'https://saleapp-crmrevo.apthai.com/api/Agreement/',
 clientId: 'crmdigital',
 clientSecret: 'pVcySkP6M2QFYvJm5h7fCjSQPoJkUIOmA1OBqZebj4orj8OX6E1U4xViZTu7VBxGKXcvmEiyipm7PO8HQfNPqZGv6v6WTQ',
+
+// ===============================DEV===============================//
+// production: false,
+// apiUrl: 'http://happyrefund-api-testrepo.devops-app.apthai.com/api/v1',
+// apiCRM: 'http://crmrevo-identity-api-crmrevo-dev.devops-app.apthai.com/api/Token/ClientLogin',
+// apiCRMMaster: 'http://crmrevo-masterdata-api-crmrevo-dev.devops-app.apthai.com/api/MasterCenters/BankBranchBOTDropdownList',
+// apiCRMRefund: 'http://crmrevo-refund-api-crmrevo-dev.devops-app.apthai.com/api/Refund/',
+// apiCRMTransferPromotion: 'http://crmrevo-promotion-api-crmrevo-dev.devops-app.apthai.com/api/TransferPromotions/',
+// apiCRMSalePromotion: 'http://crmrevo-sale-api-crmrevo-dev.devops-app.apthai.com/api/Agreement/',
+// clientId: 'crmdigital',
+// clientSecret: 'pVcySkP6M2QFYvJm5h7fCjSQPoJkUIOmA1OBqZebj4orj8OX6E1U4xViZTu7VBxGKXcvmEiyipm7PO8HQfNPqZGv6v6WTQ',
 
 // apiUrl: 'http://happyrefundapi-happyrefunds.devops-app.apthai.com/api/v1'
 };
